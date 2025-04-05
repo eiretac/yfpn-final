1e7ccf2c0fd1654c5bdac7ec31cbbec89f9df423
eiretac <95105801+eiretac@users.noreply.github.com>
2025-04-05 18:38:45 +0100

    Update Header.tsx

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -3,26 +3,37 @@ import { Link } from '@/src/navigation'
 import { useTranslations } from 'next-intl'
 import { FC } from 'react'
 import GithubIcon from '../../icons/github'
-import LogoIcon from '../../icons/logo'
+import Image from 'next/image' // Changed from LogoIcon
 import LangSwitcher from './LangSwitcher'
 import ThemeSwitch from './ThemeSwitch'
+
 interface Props {
   locale: string
 }
+
 export const Header: FC<Props> = ({ locale }) => {
   const t = useTranslations('')
   return (
-    <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5'>
+    <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5 bg-[#f9f8f5]'> {/* Added background */}
       <Link lang={locale} href='/'>
         <div className='flex flex-row items-center'>
-          <div className='mb-2 h-14 w-14'>
-            <LogoIcon />
+          {/* Replaced LogoIcon with Image component */}
+          <div className='mb-2 h-14 w-14 relative'>
+            <Image
+              src="/logo.png"
+              alt="YFPN Logo"
+              fill
+              className="object-contain dark:invert-0"
+            />
           </div>
-          <strong className='mx-2 select-none'>Template</strong>
+          {/* Updated text color */}
+          <strong className='mx-2 select-none text-[#0d294b] text-xl'>
+            Youth Foreign Policy Network
+          </strong>
         </div>
       </Link>
       <div className='flex flex-row items-center gap-3'>
-        <nav className='mr-10 inline-flex gap-5'>
+        <nav className='mr-10 inline-flex gap-5 text-[#0d294b]'> {/* Text color */}
           <Link lang={locale} href={`/about`}>
             {t('About')}
           </Link>
@@ -36,7 +47,7 @@ export const Header: FC<Props> = ({ locale }) => {
           target='_blank'
         >
           <div className='size-8'>
-            <GithubIcon />
+            <GithubIcon className="text-[#0d294b]" /> {/* Color update */}
           </div>
         </a>
       </div>
